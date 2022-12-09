818ff77bbe24ad2eca80e717f43baef3c7eea733
Leon Sloma <psyls8@exmail.nottingham.ac.uk>
2022-12-09 17:17:42 +0000

    Implements gravity flipping mechanic

---
+++ Team1Game/Assets/Scripts/GravitySwitch.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySwitch : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        print("triggered");
+
+        if(col.tag == "Player")
+        {
+            col.GetComponentInParent<PlayerController>().SetInteractable(this);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        print("untriggered");
+
+        if(col.tag == "Player")
+        {
+            col.GetComponentInParent<PlayerController>().ClearInteractable();
+        }
+    }
+
+    public void Interact(PlayerController p)
+    {
+        p.FlipGravity();
+    }
+}
