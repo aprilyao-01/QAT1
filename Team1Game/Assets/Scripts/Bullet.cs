c2d000a5f2d19185f713293a0014a4320005561e
April Yao <scysy2@nottingham.ac.uk>
2023-01-12 20:48:30 +0000

    reconstruct Bullet class to be testable

--- Team1Game/Assets/Scripts/Bullet.cs
+++ Team1Game/Assets/Scripts/Bullet.cs
@@ -2,37 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//public class Bullet : MonoBehaviour
+//{
+//    public Transform overlapFront;
+//    public float speed = 10;
+//    public bool faceLeft = false;
+//    public LayerMask groundLayer;
+//    Rigidbody2D body;
+
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        if (faceLeft) speed *= -1;
+
+//        body = GetComponent<Rigidbody2D>();
+
+
+//        print("Bullet");
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        Vector3 vel = new Vector3();
+//        vel.x += speed;
+//        body.velocity = vel;
+//        bool frontCollision = Physics2D.OverlapPoint(overlapFront.position);
+
+//        if (frontCollision)
+//        {
+//            Destroy(this.gameObject);
+//        }
+//    }
+
+//}
+
+
 public class Bullet : MonoBehaviour
 {
+    private IBulletMovement _bulletMovement;
+
     public Transform overlapFront;
     public float speed = 10;
     public bool faceLeft = false;
     public LayerMask groundLayer;
     Rigidbody2D body;
 
+    public Bullet(IBulletMovement bulletMovement)
+    {
+        _bulletMovement = bulletMovement;
+    }
 
-    // Start is called before the first frame update
     void Start()
     {
-        if(faceLeft) speed *= -1;
+        if (faceLeft) speed *= -1;
 
         body = GetComponent<Rigidbody2D>();
-        
 
         print("Bullet");
+
+        _bulletMovement = new BulletMovement(overlapFront, speed, body);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 vel = new Vector3();
-        vel.x += speed;
-        body.velocity = vel;
-        bool frontCollision = Physics2D.OverlapPoint(overlapFront.position);
-
-        if(frontCollision)
+        bool frontCollision = _bulletMovement.Move();
+        if (frontCollision)
         {
             Destroy(this.gameObject);
         }
     }
 }
+
---
+++ Team1Game/Assets/Tests/PlayMode/PlayerControllTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerControllTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void PlayerControllTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator TestOnMove_Horizontal_Input()
+    {
+        var player = new GameObject().AddComponent<PlayerController>();
+        player.speed = 5;
+        yield return null;
+
+        Assert.AreEqual(5, player.transform.position.x, 0.1f);
+    }
+}
