2720c1303343422f873598d68bcd44482243e352
April Yao <scysy2@nottingham.ac.uk>
2023-01-13 15:41:03 +0000

    Fix merge conflicts

--- Team1Game/Assets/Scripts/Crow.cs
+++ Team1Game/Assets/Scripts/Crow.cs
@@ -22,43 +22,5 @@ public class Crow : ContactDamage
     {
         crowMovement.Move();
 
-<<<<<<< HEAD
     }
 }
-=======
-        if (frontCollision)
-        {
-            Vector3 s = transform.localScale;
-            Vector3 vel = body.velocity;
-
-            s.x *= -1;
-            vel.x *= -1;
-
-            transform.localScale = s;
-            body.velocity = vel;
-        }
-    }
-}
-
-//public class Crow : MonoBehaviour
-//{
-//    public Transform overlapFront;
-//    public float speed = 10;
-
-//    Rigidbody2D body;
-
-//    private CrowMovement crowMovement;
-
-//    void Start()
-//    {
-//        body = GetComponent<Rigidbody2D>();
-//        crowMovement = new CrowMovement(overlapFront, speed, body);
-//        crowMovement.Initialise(speed);
-//    }
-
-//    void Update()
-//    {
-//        crowMovement.Move();
-//    }
-//}
->>>>>>> 85253ff (Fix merge conflicts)
